9224edcca91480dca689cb8bb39feb3e80a55db3
lkandasubbu <loganatha-raja.kandasubbu@dxc.com>
2020-09-12 12:14:33 +0530

    updated new files

---
+++ Util/dxcmode.sh
@@ -0,0 +1,35 @@
+# to kill processes highcpu_usage.sh 
+#!/bin/sh
+
+for process in `ps -ax | grep /home/lraja/Util/highcpu_usage.sh | awk '{print $1}' `
+do
+  kill -9 $process
+  echo "highcpu_usage.sh $process killed "
+done
+
+
+for process in `ps -ax | grep 'sleep 1000' | awk '{print $1}' `
+do
+  kill -9 $process
+  echo "Sleep process $process killed "
+done
+
+killall cairo-dock
+# killall /opt/whatsapp-desktop/WhatsApp
+killall radiotray
+# killall /opt/whatsapp-desktop/WhatsApp
+killall skype
+killall /usr/share/skypeforlinux/skypeforlinux
+killall nemo
+killall leafpad
+killall gnome-do gnome-calculator tomboy skype  pidgin  nemo
+killall gnome-terminal chrome  gnome-terminal-server tomboy dropbox leafpad
+killall tomboy
+# killall gnome-system-monitor
+killall radio-tray
+killall /usr/lib/firefox/firefox
+killall googleearth-bin
+killall google-earth-pro
+killall zoom
+# killall teams
+# killall team
