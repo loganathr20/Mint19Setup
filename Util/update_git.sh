30d4713031c26510afd7dd7cbb88933e26aa9a6e
loganathr20 <loganathr20@github.com>
2024-01-27 18:22:36 +0530

    added

---
+++ Util/update_git.sh
@@ -0,0 +1,28 @@
+
+
+cd /home/lraja/Desktop/Mint19Setup
+git status
+git pull
+
+cd /home/lraja/Github/Mint19Setup
+git status
+git pull
+
+cd /home/lraja/Desktop/PrivateRepo
+git status
+git pull
+
+cd /home/lraja/Desktop/Kanda
+git status
+git pull
+
+cd /home/lraja/Desktop/Garage
+git status
+git pull
+
+cd /home/lraja/Desktop/kandasubbu
+git status
+git pull
+
+sleep 30
+
