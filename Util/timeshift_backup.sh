0be7723b8d2cc18c0c92bc34e6f7f3a159076807
loganathr20 <loganathr20@github.com>
2024-01-28 17:05:59 +0530

    added new file

---
+++ Util/timeshift_backup.sh
@@ -0,0 +1,6 @@
+
+
+cp -R /run/timeshift/6198/backup/timeshift/snapshots/*  /media/lraja/Elephant/LinuxMint_Downloads/Timeshift_backups/
+
+notify-send "Message from cron ::: Timeshift Backup completed.  /media/lraja/Elephant/LinuxMint_Downloads/Timeshift_backups/ "
+
