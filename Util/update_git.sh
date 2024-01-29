e02941115a30624099bdea25cb1e71023599cca1
loganathr20 <loganathr20@github.com>
2024-01-29 11:23:58 +0530

    updated

--- Util/update_git.sh
+++ Util/update_git.sh
@@ -1,37 +1,58 @@
+clear
 
+echo "Updating Git Repositories locally"
+echo "========================================"
+echo "\n\n"
 
+echo "Updating Desktop/Mint19Setup"
 cd /home/lraja/Desktop/Mint19Setup
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Github/Mint19Setup"
 cd /home/lraja/Github/Mint19Setup
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Desktop/PrivateRepo"
 cd /home/lraja/Desktop/PrivateRepo
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Desktop/Kanda"
 cd /home/lraja/Desktop/Kanda
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Desktop/Garage"
 cd /home/lraja/Desktop/Garage
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Documents/Deploy.tomcat.web-project"
 cd /home/lraja/Documents/Deploy.tomcat.web-project
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Desktop/Deploy.tomcat.web-project"
 cd /home/lraja/Desktop/Deploy.tomcat.web-project
 git status
 git pull
 
+echo "\n\n"
+echo "Updating Documents/Deploy.Kubernetes.web-project"
 cd /home/lraja/Documents/Deploy.Kubernetes.web-project
 git status
 git pull
 
+# echo "\n\n"
+# echo "Updating Desktop/kandasubbu"
 # cd /home/lraja/Desktop/kandasubbu
 # git status
 # git pull
