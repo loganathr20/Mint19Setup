8f3b4c52c23cf49e56a4691831fc9f382a2524f7
loganathr20 <loganathr20@github.com>
2024-02-02 12:16:13 +0530

    update

--- Util/upt.sh
+++ Util/upt.sh
@@ -22,23 +22,23 @@ do
 #       sudo hddtemp /dev/sda
 #       sudo sensors
  
-        echo  "${GREEN} System Uptime :  "
+        echo -e "${GREEN} System Uptime :  "
         echo " "
         uptime
         echo " "
-        echo  " ${RESET}"
+        echo -e " ${RESET}"
         
         echo "--------------------------------------------------------------"
-        echo  "${YELLOW}  Network Speed "
+        echo -e "${YELLOW}  Network Speed "
         echo " "
 	    curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -
         echo "--------------------------------------------------------------"
         echo " "
-        echo  " ${RESET} "
+        echo -e " ${RESET} "
 
-	    echo  "${RED} Running in Background. Hit [CTRL+C] to stop! ${RESET} "
+	    echo -e "${RED} Running in Background. Hit [CTRL+C] to stop! ${RESET} "
         echo "--------------------------------------------------------------"
-        sleep 58m
+        sleep 30m
 
 done
 
