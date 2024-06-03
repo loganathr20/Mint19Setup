afffef1098970124caa54f61b0991105605e1701
loganathr20 <loganathr20@github.com>
2024-06-03 09:42:37 +0530

    Timeshift backup tool

---
+++ Util/TS_Backup.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# mikes handy Timeshift filesystem-snapshot utility
+# ----------------------------------------------------------------------
+# this needs to be a lot more general, but the basic idea is it makes
+# rotating Timeshift backup-snapshots of /home whenever called
+# ----------------------------------------------------------------------
+
+
+
+sudo timeshift --list
+
+
+
+sudo timeshift --check
+
+
+sudo timeshift --create
+
+
+sudo timeshift --list
