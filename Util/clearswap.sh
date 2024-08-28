98621585af81c7007dfffff30dadd66847cb5134
loganathr20 <loganathr20@github.com>
2024-08-28 14:44:25 +0530

    updae

--- Util/clearswap.sh
+++ Util/clearswap.sh
@@ -6,46 +6,61 @@
 
 clear 
 
-echo -e "\e[31;43m***** HOSTNAME INFORMATION *****\e[0m"
+echo "\e[31;43m***** HOSTNAME INFORMATION *****\e[0m"
 hostnamectl
 echo ""
 
 # -File system disk space usage:
-echo -e "\e[31;43m***** FILE SYSTEM DISK SPACE USAGE *****\e[0m"
+echo "\e[31;43m***** FILE SYSTEM DISK SPACE USAGE *****\e[0m"
 df -h
 echo ""
-# -Free and used memory in the system:
-echo -e "\e[31;43m ***** FREE AND USED MEMORY *****\e[0m"
-free
-echo ""
+
 # -System uptime and load:
-echo -e "\e[31;43m***** SYSTEM UPTIME AND LOAD *****\e[0m"
+echo "\e[31;43m***** SYSTEM UPTIME AND LOAD *****\e[0m"
 uptime
 echo ""
 # -Logged-in users:
-echo -e "\e[31;43m***** CURRENTLY LOGGED-IN USERS *****\e[0m"
+echo "\e[31;43m***** CURRENTLY LOGGED-IN USERS *****\e[0m"
 who
 echo ""
 # -Top 5 processes as far as memory usage is concerned
-echo -e "\e[31;43m***** TOP 5 MEMORY-CONSUMING PROCESSES *****\e[0m"
+echo "\e[31;43m***** TOP 5 MEMORY-CONSUMING PROCESSES *****\e[0m"
 ps -eo %mem,%cpu,comm --sort=-%mem | head -n 6
 echo ""
-echo -e "\e[1;32mDone.\e[0m"
+echo "\e[1;32mDone.\e[0m"
+echo ""
+echo ""
+echo ""
+echo ""
 
-sleep 10
-
-echo -e "\e[31;43m***** Clearing Swap Space *****\e[0m"
-
-swapoff -a
-
-sleep 30
-
-swapon -a
-
-sleep 10
-
-clear
-
-echo -e "\e[31;43m ***** FREE AND USED MEMORY *****\e[0m"
+# -Free and used memory in the system:
+echo "\e[31;43m ***** FREE AND USED MEMORY *****\e[0m"
 free
 echo ""
+
+
+echo  "\033[0;34m ***********************************************************************************************\e[0m"
+
+echo  "\e[0;34m **********   PLEASE WAIT SOMETIME FOR SWAP SPACE CLEANUP TO COMPLETE . **********\e[0m"
+echo ""
+echo ""
+echo  "\033[0;34m ***** Clearing Swap Space *****\e[0m"
+swapoff -a
+sleep 40
+swapon -a
+sleep 10
+echo ""
+echo ""
+echo ""
+echo  "\033[0;34m **********  SWAP SPACE CLEANUP COMPLETED . **********\e[0m"
+echo ""
+echo ""
+echo  "\033[0;34m ***********************************************************************************************\e[0m"
+
+echo "\e[31;43m ***** FREE AND USED MEMORY *****\e[0m"
+free
+echo ""
+
+echo  "\033[0;34m ***********************************************************************************************\e[0m"
+
+
